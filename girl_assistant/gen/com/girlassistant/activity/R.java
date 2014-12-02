7a985db75e4d6ba10b55f17906af926201156385
gaojian89 <383404439@qq.com>
2014-12-02 14:03:18 +0800

    commit
    commit

--- girl_assistant/gen/com/girlassistant/activity/R.java
+++ girl_assistant/gen/com/girlassistant/activity/R.java
@@ -28,101 +28,21 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int ask_item_government=0x7f050027;
-        public static final int ask_item_type=0x7f050028;
-        public static final int ask_line=0x7f050012;
-        public static final int ask_progress_light=0x7f050039;
-        public static final int ask_progress_normal=0x7f050038;
-        public static final int ask_text_black=0x7f050016;
-        public static final int ask_text_gray=0x7f050011;
-        public static final int ask_text_pink=0x7f050013;
-        public static final int ask_text_red=0x7f050014;
-        public static final int ask_title_bar_text=0x7f050015;
-        public static final int black=0x7f050005;
-        public static final int blue=0x7f050031;
-        /**  share 
-         */
-        public static final int channel_item_textcolor=0x7f05002e;
-        public static final int comment_list_count_text_color=0x7f050008;
-        public static final int comment_list_text_color=0x7f050032;
-        public static final int comment_list_text_color_normal=0x7f050033;
-        public static final int cover1_bg=0x7f050043;
-        public static final int cover2_bg=0x7f050044;
-        public static final int cover3_bg=0x7f050045;
-        public static final int cover4_bg=0x7f050046;
-        public static final int default_text=0x7f050026;
-        /**  fragment 
-         */
-        public static final int fragment_middle=0x7f05003d;
-        public static final int gov_tab_text_color=0x7f050040;
-        public static final int gov_tab_text_color_p=0x7f05003f;
-        public static final int help_raise_feedback=0x7f050049;
-        /** 帮  
-         */
-        public static final int help_raise_going_on=0x7f050047;
-        public static final int help_raise_the_end=0x7f050048;
-        public static final int home_gray=0x7f05000e;
-        public static final int home_red=0x7f05000f;
-        public static final int item_divid_line_down=0x7f05000c;
-        public static final int item_divid_line_up=0x7f05000b;
-        /**  下拉上拉刷新webview 
- <color name="gray">#aaaaaa</color> 
-         */
-        public static final int light_blue=0x7f050034;
-        public static final int listen_red_line=0x7f050036;
-        /**  音频专题 
-         */
-        public static final int listen_red_title=0x7f050035;
-        public static final int listen_subject_detail_info=0x7f050037;
-        /**  ask 
-         */
-        public static final int listview_divider_line=0x7f050010;
-        public static final int main_tab_account_background=0x7f05000d;
-        public static final int main_tab_comment_color=0x7f050009;
-        public static final int main_tab_divid_line=0x7f05000a;
-        public static final int main_title=0x7f05003e;
+        public static final int ask_item_type=0x7f050004;
+        public static final int black=0x7f050003;
+        public static final int blue=0x7f050005;
+        public static final int cover1_bg=0x7f050007;
+        public static final int cover2_bg=0x7f050008;
+        public static final int cover3_bg=0x7f050009;
+        public static final int cover4_bg=0x7f05000a;
+        public static final int light_blue=0x7f050006;
         public static final int night_mode=0x7f050000;
-        public static final int night_row_title_text_color=0x7f05002b;
-        public static final int paper_tab_bg_normal=0x7f05002f;
-        public static final int paper_tab_bg_press=0x7f050030;
-        public static final int people_list_item_tips_color=0x7f050007;
-        /**  setting 
-         */
-        public static final int setting_bg=0x7f05002a;
-        public static final int setting_font_line_gray=0x7f05002c;
-        public static final int setting_font_line_red=0x7f05002d;
-        public static final int shallow_gray=0x7f050006;
-        public static final int share_green=0x7f050003;
-        /**  透明色 
-         */
-        public static final int share_red=0x7f050002;
-        public static final int subscribe_item_disabled_bg=0x7f05001d;
-        public static final int subscribe_item_disabled_stroke=0x7f05001c;
-        public static final int subscribe_item_drag_bg=0x7f050017;
-        public static final int subscribe_item_drag_stroke=0x7f050018;
-        public static final int subscribe_item_normal_bg=0x7f050020;
-        public static final int subscribe_item_normal_stroke=0x7f05001f;
-        public static final int subscribe_item_pressed_bg=0x7f05001e;
-        public static final int subscribe_item_pressed_stroke=0x7f05001a;
-        public static final int subscribe_item_selected_bg=0x7f05001b;
-        public static final int subscribe_item_selected_stroke=0x7f050019;
-        /**  <item name="subscribe_item_text_color_normal" type="color">@color/default_text</item> 
-         */
-        public static final int subscribe_item_text_color_normal=0x7f050029;
-        public static final int subscribe_item_text_color_pressed=0x7f050021;
-        public static final int subscribe_province_selected_bg=0x7f050025;
-        public static final int subscribe_seperate_line=0x7f050023;
-        public static final int subscribe_tab_normal=0x7f050024;
-        public static final int subscribe_tip_text=0x7f050022;
-        public static final int tab_item_text_line=0x7f05003c;
-        /**  tab 
-         */
-        public static final int tab_item_text_normal=0x7f05003a;
-        public static final int tab_item_text_selected=0x7f05003b;
+        public static final int powder=0x7f05000b;
         public static final int transparent=0x7f050001;
-        public static final int user_nickname_noti=0x7f050042;
-        public static final int video_bg=0x7f050041;
-        public static final int white=0x7f050004;
+        public static final int white=0x7f050002;
+    }
+    public static final class dimen {
+        public static final int text_size_home_bottom=0x7f060000;
     }
     public static final class drawable {
         public static final int cover_0=0x7f020000;
@@ -138,13 +58,19 @@ public final class R {
         public static final int pic_default=0x7f02000a;
     }
     public static final class id {
-        public static final int cover_layout=0x7f080005;
-        public static final int guide_item_layout=0x7f080002;
-        public static final int guide_page_act_index=0x7f080001;
-        public static final int guide_page_act_viewpager=0x7f080000;
-        public static final int guide_page_item_btn=0x7f080003;
-        public static final int guide_page_item_image=0x7f080004;
-        public static final int loading_act_image=0x7f080006;
+        public static final int cover_layout=0x7f09000b;
+        public static final int guide_item_layout=0x7f090002;
+        public static final int guide_page_act_index=0x7f090001;
+        public static final int guide_page_act_viewpager=0x7f090000;
+        public static final int guide_page_item_btn=0x7f090003;
+        public static final int guide_page_item_image=0x7f090004;
+        public static final int home_act_bottom_menu=0x7f090005;
+        public static final int home_act_container=0x7f09000a;
+        public static final int home_act_home_page=0x7f090006;
+        public static final int home_act_personal_center=0x7f090008;
+        public static final int home_act_release=0x7f090009;
+        public static final int home_act_square=0x7f090007;
+        public static final int loading_act_image=0x7f09000c;
     }
     public static final class layout {
         public static final int guide_page_act=0x7f030000;
@@ -153,10 +79,14 @@ public final class R {
         public static final int loading_act=0x7f030003;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int content_description=0x7f060001;
-        public static final int error_web_notify_text=0x7f060003;
-        public static final int processing=0x7f060002;
+        public static final int app_name=0x7f070000;
+        public static final int content_description=0x7f070001;
+        public static final int error_web_notify_text=0x7f070003;
+        public static final int home_page=0x7f070004;
+        public static final int personal_center=0x7f070006;
+        public static final int processing=0x7f070002;
+        public static final int release=0x7f070007;
+        public static final int square=0x7f070005;
     }
     public static final class style {
         /** 
@@ -171,9 +101,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070001;
+        public static final int AppBaseTheme=0x7f080001;
         /**  Application theme. 
          */
-        public static final int Theme_AppTheme=0x7f070000;
+        public static final int Theme_AppTheme=0x7f080000;
     }
 }
---
+++ girl_assistant/src/com/girlassistant/activity/ui/UIBaseActivity.java
@@ -0,0 +1,12 @@
+package com.girlassistant.activity.ui;
+
+import android.os.Bundle;
+
+public class UIBaseActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
